ff8a38bea0c6884429f7605ec7df3a3ae3f97e03
danil <monastyrevdanil@mail.ru>
2024-05-24 16:57:18 +0300

    первый коммит

---
+++ consoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Student\Desktop\number.txt";
+            WriteNumbersToFile(path);
+
+            Console.ReadKey(true);
+        }
+
+        public static void WriteNumbersToFile(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                using (StreamWriter sw = new StreamWriter(fs, Encoding.Default))
+                {
+                    for (int i = 1; i <= 500; i++)
+                        sw.Write($"{i}, ");
+                }
+            }
+        }
+    }
+}
